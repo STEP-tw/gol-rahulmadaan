116c36d06b7f3c7e6c49349f0b1f699fa5e3771d
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-11-29 10:11:11 +0530

    Code beautified, missing semi-colons added

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -1,11 +1,14 @@
-let { daysIteration,world, getCoordOfAliveCells} = require('./library.js');
+const { daysIteration,
+  world,
+  getCoordOfAliveCells
+} = require('./library.js');
 
-const nextGeneration = function(currGeneration,bounds) {
-  let result = [];
-  let dimensionOfWorld = (bounds.bottomRight[0] - bounds.topLeft[0] + 1);
-  result =  world(dimensionOfWorld,currGeneration);
-  result = daysIteration(result);
-  return getCoordOfAliveCells(result);
-}
+const nextGeneration = function(currGeneration, bounds) {
+    let result = [];
+    let dimensionOfWorld = (bounds.bottomRight[0] - bounds.topLeft[0] + 1);
+    result = world(dimensionOfWorld, currGeneration);
+    result = daysIteration(result);
+    return getCoordOfAliveCells(result);
+};
 
 module.exports = { nextGeneration };
